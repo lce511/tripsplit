83550b4333855f28f48be693db62a53acaff7d9c
lce511 <iris0511smile@gmail.com>
2026-09-10 14:43:46 +0800

    Add Supabase config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.TRIPSPLIT_CONFIG = {
+  SUPABASE_URL: "https://lxmisefnrlhedwynbnmn.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_4-UHB6FCXKbWn7NhOFSOyA_Mvdwglyo",
+  TRIP_NAME: "LA Trip 2026"
+};
